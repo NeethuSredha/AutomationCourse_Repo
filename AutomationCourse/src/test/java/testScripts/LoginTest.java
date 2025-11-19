06f6319a4c6f02a1e37924d04a7edebd3d554407
Lenovo <Lenovo@192.168.1.36>
2025-11-19 21:41:53 +0530

    change in webelement commit.

--- AutomationCourse/src/test/java/testScripts/LoginTest.java
+++ AutomationCourse/src/test/java/testScripts/LoginTest.java
@@ -9,49 +9,49 @@ import seleniumBasics.DemoBase;
 public class LoginTest extends DemoBase{
 	@Test
 	public void verifyWhetherUserIsAbleToLoginWithValidCredential() {
-		WebElement txtUserName=driver.findElement(By.id("user-name"));
-		txtUserName.sendKeys("standard_user");
+		WebElement userName=driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
 		
-		WebElement txtPassword=driver.findElement(By.id("password"));
-		txtPassword.sendKeys("secret_sauce");
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
 		
-		WebElement buttonLogin=driver.findElement(By.id("login-button"));
-		buttonLogin.click();
+		WebElement loginbutton=driver.findElement(By.id("login-button"));
+		loginbutton.click();
 	}
 	
 	@Test
 	public void verifyWhetherUserIsAbleToLoginWithValidUsernameAndInValidPassword() {
-		WebElement txtUserName=driver.findElement(By.id("user-name"));
-		txtUserName.sendKeys("locked_out_user");
+		WebElement userName=driver.findElement(By.id("user-name"));
+		userName.sendKeys("locked_out_user");
 		
-		WebElement txtPassword=driver.findElement(By.id("password"));
-		txtPassword.sendKeys("secret_sauce1");
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce1");
 		
-		WebElement buttonLogin=driver.findElement(By.id("login-button"));
-		buttonLogin.click();
+		WebElement loginbutton=driver.findElement(By.id("login-button"));
+		loginbutton.click();
 	}
 	
 	@Test
 	public void verifyWhetherUserIsAbleToLoginWithInValidUsernameAndValidPassword() {
-		WebElement txtUserName=driver.findElement(By.id("user-name"));
-		txtUserName.sendKeys("locked_out_user1");
+		WebElement userName=driver.findElement(By.id("user-name"));
+		userName.sendKeys("locked_out_user1");
 		
-		WebElement txtPassword=driver.findElement(By.id("password"));
-		txtPassword.sendKeys("secret_sauce");
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
 		
-		WebElement buttonLogin=driver.findElement(By.id("login-button"));
-		buttonLogin.click();
+		WebElement loginbutton=driver.findElement(By.id("login-button"));
+		loginbutton.click();
 	}
 	
 	@Test
 	public void verifyWhetherUserIsAbleToLoginWithInValidUsernameAndInValidPassword() {
-		WebElement txtUserName=driver.findElement(By.id("user-name"));
-		txtUserName.sendKeys("problem_user2");
+		WebElement userName=driver.findElement(By.id("user-name"));
+		userName.sendKeys("problem_user2");
 		
-		WebElement txtPassword=driver.findElement(By.id("password"));
-		txtPassword.sendKeys("secret_sauce2");
+		WebElement password=driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce2");
 		
-		WebElement buttonLogin=driver.findElement(By.id("login-button"));
-		buttonLogin.click();
+		WebElement loginbutton=driver.findElement(By.id("login-button"));
+		loginbutton.click();
 	}
 }
