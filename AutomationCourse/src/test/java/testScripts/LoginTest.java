6eca450c293e0e0e78a6b4215acc65024c712578
Lenovo <Lenovo@192.168.1.36>
2025-11-20 20:16:44 +0530

    POM with PageFactory commit.

--- AutomationCourse/src/test/java/testScripts/LoginTest.java
+++ AutomationCourse/src/test/java/testScripts/LoginTest.java
@@ -4,19 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import pages.LoginPage;
 import seleniumBasics.DemoBase;
 
 public class LoginTest extends DemoBase{
 	@Test
 	public void verifyWhetherUserIsAbleToLoginWithValidCredential() {
-		WebElement userName=driver.findElement(By.id("user-name"));
-		userName.sendKeys("standard_user");
-		
-		WebElement password=driver.findElement(By.id("password"));
-		password.sendKeys("secret_sauce");
-		
-		WebElement loginbutton=driver.findElement(By.id("login-button"));
-		loginbutton.click();
+		LoginPage loginPage=new LoginPage(driver);
+		loginPage.enterUserNameOnUserNameField();
+		loginPage.enterPasswordOnPasswordField();
+		loginPage.loginButtonClick();
 	}
 	
 	@Test
