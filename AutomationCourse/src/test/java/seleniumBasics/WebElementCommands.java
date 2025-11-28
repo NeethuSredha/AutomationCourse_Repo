1f289c781b6e77afc7b24e0e22eae48686790435
Lenovo <Lenovo@192.168.1.36>
2025-11-28 20:56:21 +0530

    fluent wait.

--- AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -1,25 +1,41 @@
 package seleniumBasics;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import java.time.Duration;
+import java.util.NoSuchElementException;
 
-public class WebElementCommands extends BasePgm{
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebElementCommands extends BasePgm {
 	public void verifyWebElementCommands() {
-	driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
-	WebElement messageTextBox=driver.findElement(By.id("single-input-field"));
-	messageTextBox.sendKeys("Neethu");
-	WebElement showMessagebutton=driver.findElement(By.id("button-one"));
-	showMessagebutton.click();
-	System.out.println(showMessagebutton.isDisplayed());
-	System.out.println(showMessagebutton.isEnabled());
-	WebElement text=driver.findElement(By.id("message-one"));
-	System.out.println(text.getText());
-	System.out.println(text.getTagName());
-	messageTextBox.clear();
-	System.out.println(showMessagebutton.getCssValue("background-color"));
-}
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement messageTextBox = driver.findElement(By.id("single-input-field"));
+		messageTextBox.sendKeys("Neethu");
+		WebElement showMessagebutton = driver.findElement(By.id("button-one"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showMessagebutton));
+		
+		Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		fluentWait.until(ExpectedConditions.elementToBeClickable(showMessagebutton));
+		
+		showMessagebutton.click();
+		System.out.println(showMessagebutton.isDisplayed());
+		System.out.println(showMessagebutton.isEnabled());
+		WebElement text = driver.findElement(By.id("message-one"));
+		System.out.println(text.getText());
+		System.out.println(text.getTagName());
+		messageTextBox.clear();
+		System.out.println(showMessagebutton.getCssValue("background-color"));
+	}
+
 	public static void main(String[] args) {
-		WebElementCommands webElementCommands=new WebElementCommands();
+		WebElementCommands webElementCommands = new WebElementCommands();
 		webElementCommands.initializeBrowser();
 		webElementCommands.verifyWebElementCommands();
 
